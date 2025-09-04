1f2d9ef02e296a87f6b6159e3218c5d1b95ac50b
marlowX <biuro@alpmeb.pl>
2025-09-04 14:47:45 +0200

    feat: Operator piły ma dostęp do pełnych szczegółów ZKO
    - Kliknięcie na numer ZKO otwiera pełną stronę szczegółów
    
    - Operator może zarządzać pozycjami i paletami używając istniejących komponentów
    
    - Dodano routing /worker/zko/:id dla szczegółów ZKO
    
    - Wykorzystuje istniejący komponent ZKODetailsPage z pełną funkcjonalnością

--- commit-menu-update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd D:\PROJEKTY\PROGRAMOWANIE\AlpApp
-git add .
-git commit -m "feat: Add Worker Panels to navigation menu" -m "- Added 'Panele pracownikow' section in sidebar" -m "- Links to Pila and Okleiniarka panels are active" -m "- Other panels marked as disabled (coming soon)"
-git push
-echo.
-echo Commit completed!
-pause
--- create_branch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd D:\PROJEKTY\PROGRAMOWANIE\AlpApp
-git checkout -b feature/worker-views-zko
-echo Branch created: feature/worker-views-zko
